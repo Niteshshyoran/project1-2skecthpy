2f5974feeaf0f3aafabde1c85ac473f71565adac
Niteshshyoran <139843441+Niteshshyoran@users.noreply.github.com>
2023-07-21 19:44:06 +0530

    Add files via upload
    file sketchpy

---
+++ project2.py
@@ -0,0 +1,15 @@
+# from sketchpy import canvas
+# # import sketchpy
+# obj = canvas.sketch_from_image(r"D:\youtube pic\CSK vs RCB\4c84300c-ef79-45f5-b576-d63bc6b96bd1.webp")
+# obj.draw(threshold=100)
+
+
+
+# from sketchpy import library as lib
+# obj = lib.apj()
+# obj.draw()
+
+from sketchpy import library as lib
+obj = lib.flag()
+obj.draw()
+
